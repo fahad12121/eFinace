8e996036a8150e936d5dd3373248592ddfb81e97
Fahad Aslam <58298975+fahad12121@users.noreply.github.com>
2025-04-30 08:51:28 +0500

    change

--- cronjob/uploadDB.js
+++ cronjob/uploadDB.js
@@ -14,34 +14,6 @@ cron.schedule('0 5 * * *', async () => {
     console.log('Starting MySQL Backup...');
 
     const date = new Date().toISOString().split('T')[0];  // Get current date in YYYY-MM-DD format
-    let backupFilePath = `efinance_${date}.sql`;
-
-    // Backup MySQL Database
-    backupDatabase(dbHost, username, password, dbName, backupFilePath);
-    // Path to your existing backup file
-
-    backupFilePath = `../efinance_2025-04-25.sql`;
-
-    // Upload to Google Drive
-    setTimeout(function () {
-      uploadFileToDrive(backupFilePath, '1w_6YIxrNWunnUkMRrqxnLJqH5qxOjDAP');
-      console.log('Backup uploaded successfully to Google Drive.');
-
-    }, 50000);
-
-  } catch (error) {
-    console.error('Error during backup or upload:', error);
-  }
-});
-
-
-
-// // Trigger the cron job manually for testing
-(async () => {
-  try {
-    console.log('Starting MySQL Backup...');
-
-    const date = new Date().toISOString().split('T')[0];  // Get current date in YYYY-MM-DD format
     let backupFile = `efinance_${date}.sql`;
 
 
@@ -59,4 +31,31 @@ cron.schedule('0 5 * * *', async () => {
   } catch (error) {
     console.error('Error during backup or upload:', error);
   }
-})();
+});
+
+
+
+// // Trigger the cron job manually for testing
+// (async () => {
+//   try {
+//     console.log('Starting MySQL Backup...');
+
+//     const date = new Date().toISOString().split('T')[0];  // Get current date in YYYY-MM-DD format
+//     let backupFile = `efinance_${date}.sql`;
+
+
+//     // Backup MySQL Database
+//     backupDatabase(dbHost, username, password, dbName, backupFile);
+
+//     console.log('Backup created, uploading to Google Drive...');
+
+//     // Upload to Google Drive
+//     setTimeout(function () {
+//       uploadFileToDrive(backupFile, '1w_6YIxrNWunnUkMRrqxnLJqH5qxOjDAP');
+
+//       console.log('Backup uploaded successfully to Google Drive.');
+//     }, 30000);
+//   } catch (error) {
+//     console.error('Error during backup or upload:', error);
+//   }
+// })();
